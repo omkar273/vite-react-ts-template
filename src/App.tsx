c1a383b3b3533eed53c43fc0c84182f2fdd2994f
omkar273 <omkarsonawane622@gmail.com>
2024-03-14 00:49:08 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="font-fira-sans">App</div>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    manifest: true,
+  },
+  plugins: [react()],
+  server: {
+    cors: true,
+    port: 3000,
+    origin: "http://localhost:3000",
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
